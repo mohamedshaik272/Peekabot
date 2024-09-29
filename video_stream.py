9f4624c3a3ebb645ee2f8f8964cd08205200e592
Udit Verma <udiverma@outlook.com>
2024-09-29 02:25:09 -0400

    updates 123123123

--- video_stream.py
+++ video_stream.py
@@ -18,7 +18,7 @@ def generate_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/video_feed')
+@app.route('/')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
